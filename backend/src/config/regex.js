2ab059729c87db9c844949466c0438f5a3260bea
DUBOSCLARD LÉO <leo.dubosclard@epitech.eu>
2022-03-20 15:26:46 +0100

    backend push -> regex on email

---
+++ backend/src/config/regex.js
@@ -0,0 +1,3 @@
+exports.valideEmail = new RegExp(
+    '^[a-zA-Z0-9._:$!%-]+@[a-zA-Z0-9.-]+.[a-zA-Z]$'
+);
--- backend/src/routes/auth/auth.js
+++ backend/src/routes/auth/auth.js
@@ -4,6 +4,7 @@ const router = express.Router();
 
 const { is_good_data } = require('../../middleware/datas');
 const { register, login } = require('../users/users.query');
+const { valideEmail } = require('../../config/regex');
 
 router.post('/register', (req, res) => {
     var firstname = req.body.firstname;
@@ -12,7 +13,7 @@ router.post('/register', (req, res) => {
     var email = req.body.email;
     var password = req.body.password;
 
-    if (!is_good_data(firstname) || !is_good_data(name) || !is_good_data(nickname) || !is_good_data(email) || !is_good_data(password) || email.indexOf('@') < 0)
+    if (!is_good_data(firstname) || !is_good_data(name) || !is_good_data(nickname) || !is_good_data(email) || !is_good_data(password) || !valideEmail.test(email))
         return res.status(500).json({ msg: 'internal server error (bad entries)' });
 
     bcrypt.hash(password, 10)
@@ -26,7 +27,7 @@ router.post('/login', (req, res) => {
     var email = req.body.email;
     var password = req.body.password;
 
-    if (!is_good_data(email) || !is_good_data(password))
+    if (!is_good_data(email) || !is_good_data(password) || !valideEmail.test(email))
         return res.status(500).json({ msg: 'internal server error' });
     login(res, email, password);
 });
