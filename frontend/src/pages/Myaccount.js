aa44c5312da163ebf04ff4b414dd99c21fa3eb39
DUBOSCLARD LÉO <leo.dubosclard@epitech.eu>
2022-04-07 18:01:59 +0200

    feat: profile picture change works + Modal to confirm that the user want to delete account
    Signed-off-by: DUBOSCLARD LÉO <leo.dubosclard@epitech.eu>

--- frontend/src/pages/Myaccount.js
+++ frontend/src/pages/Myaccount.js
@@ -4,6 +4,7 @@ import isConnected from '../js/isConnected';
 import { Navigate } from 'react-router-dom';
 import { ConnectionNavbar } from './Navbar';
 import Modal from 'react-modal';
+import { Link } from 'react-router-dom';
 
 import { toast, ToastContainer } from 'react-toastify';
 
@@ -17,8 +18,27 @@ const ModalStyle = {
         transform: 'translate(-50%, -50%)',
         backgroundColor: '#303030',
         color: '#efefef',
-        width: '50%',
+        width: '40%',
         height: '60%',
+        borderRadius: 15,
+        overflow: 'hidden',
+    },
+}
+
+const DeleteModalStyle = {
+    content: {
+        top: '50%',
+        left: '50%',
+        right: 'auto',
+        bottom: 'auto',
+        marginRight: '-50%',
+        transform: 'translate(-50%, -50%)',
+        backgroundColor: '#303030',
+        color: '#efefef',
+        width: '20%',
+        height: '20%',
+        borderRadius: 15,
+        overflow: 'hidden',
     },
 }
 
@@ -31,6 +51,7 @@ class Myaccount extends React.Component {
             user: [],
             image_url: '',
             modalIsOpen: false,
+            deleteConfirmationModal: false,
         };
         this.handleSubmitDeconnection = this.handleSubmitDeconnection.bind(this);
         this.handleSubmitDelete = this.handleSubmitDelete.bind(this);
@@ -38,6 +59,8 @@ class Myaccount extends React.Component {
         this.encodeBase64 = this.encodeBase64.bind(this);
         this.openModal = this.openModal.bind(this);
         this.closeModal = this.closeModal.bind(this);
+        this.openDeleteModal = this.openDeleteModal.bind(this);
+        this.closeDeleteModal = this.closeDeleteModal.bind(this);
     }
 
     componentDidMount() {
@@ -56,6 +79,14 @@ class Myaccount extends React.Component {
                 this.setState({ image_url: data.image_url });
             });
         }
+    }
+
+    openDeleteModal() {
+        this.setState({ deleteConfirmationModal: true });
+    }
+
+    closeDeleteModal() {
+        this.setState({ deleteConfirmationModal: false });
     }
 
     openModal() {
@@ -99,11 +130,11 @@ class Myaccount extends React.Component {
         return canvas.toDataURL('image/jpeg');
     }
 
-    handleChangeUserImage(event) {
+    handleChangeUserImage(event, url) {
         event.preventDefault();
 
-        if (event.target.files.length > 1) {
-            toast.error("Can't post more than 1 image !", {
+        if (!url || url === '') {
+            toast.error("Can't change the image !", {
                 position: "bottom-right",
                 autoClose: 5000,
                 hideProgressBar: false,
@@ -115,36 +146,21 @@ class Myaccount extends React.Component {
             return;
         }
 
-        const file = event.target.files[0];
+        let image_body = {
+            image_url: url,
+        };
 
-        if (this.isFileImage(file)) {
-
-            let image_body = {
-                image_url: this.encodeBase64(file)
-            };
-
-            fetch('http://localhost:5001/user/profile_image', {
-                method: 'PUT',
-                headers: new Headers({
-                    'Authorization': `Bearer ${localStorage.getItem('user_token')}`,
-                    'Content-Type': 'application/json'
-                }),
-                mode: 'cors',
-                body: JSON.stringify(image_body)
-            }).then(() => {
-                this.setState({ image_url: this.encodeBase64(file) });
-                toast.success('Image changed !', {
-                    position: "bottom-right",
-                    autoClose: 5000,
-                    hideProgressBar: false,
-                    closeOnClick: true,
-                    pauseOnHover: false,
-                    draggable: true,
-                    progress: undefined,
-                });
-            });
-        } else {
-            toast.error('This is not an image !', {
+        fetch('http://localhost:5001/user/profile_image', {
+            method: 'PUT',
+            headers: new Headers({
+                'Authorization': `Bearer ${localStorage.getItem('user_token')}`,
+                'Content-Type': 'application/json'
+            }),
+            mode: 'cors',
+            body: JSON.stringify(image_body)
+        }).then(() => {
+            this.setState({ image_url: url });
+            toast.success('Image changed !', {
                 position: "bottom-right",
                 autoClose: 5000,
                 hideProgressBar: false,
@@ -153,7 +169,8 @@ class Myaccount extends React.Component {
                 draggable: true,
                 progress: undefined,
             });
-        }
+            this.closeModal();
+        });
     }
 
     render() {
@@ -163,28 +180,46 @@ class Myaccount extends React.Component {
             <div className='w-screen background h-screen mx-auto text-white text-2xl bg-[#141414]'>
                 <ConnectionNavbar />
                 <div className='grid place-items-center not_connected'>
-                    <div className='p-5 bg-[#303030] h-3/4 w-3/5 rounded-2xl'>
+                    <div className='p-5 bg-[#303030] h-3/4 w-1/2 rounded-2xl'>
                         <div className='grid place-items-center'>
+                            <Link to='/'>
+                                <button title='Back to home' className='bg-white rounded p-5 text-black text-2xl hover:bg-gray-300 hover:opacity-70'>NetFlex Home</button>
+                            </Link>
                             <h1>Welcome back, {this.state.user.nickname} !</h1>
                             <h1>Firstname : {this.state.user.firstname}</h1>
                             <h1>Name : {this.state.user.name}</h1>
                             <h1>Email : {this.state.user.email}</h1>
                             <div>
-                                <img className='w-52 h-52' src={this.state.image_url} />
-                                <button onClick={this.openModal}>Open Modal</button>
+                                <button onClick={this.openModal} title='Change profile picture'>
+                                    <img className='w-52 h-52 hover:opacity-25' src={this.state.image_url} />
+                                </button>
                             </div>
 
                             <div className='grid grid-cols-2 space-x-5'>
-                                <button onClick={this.handleSubmitDeconnection} className='px-5 py-3 rounded-lg bg-gradient-to-r from-[#8b1418] to-[#d71f26] hover:from-[#d71f26] hover:to-[#d15156]'>
+                                <button onClick={this.handleSubmitDeconnection} title='Deconnection' className='px-5 py-3 rounded-lg bg-gradient-to-r from-[#8b1418] to-[#d71f26] hover:from-[#d71f26] hover:to-[#d15156]'>
                                     Deconnection
                                 </button>
-                                <button onClick={this.handleSubmitDelete} className='px-5 py-3 rounded-lg bg-gradient-to-r from-[#d71f26] to-[#8b1418] hover:from-[#d15156] hover:to-[#d71f26]'>
-                                    Delete
+                                <button onClick={this.openDeleteModal} title='Delete your NetFlex account' className='px-5 py-3 rounded-lg bg-gradient-to-r from-[#d71f26] to-[#8b1418] hover:from-[#d15156] hover:to-[#d71f26]'>
+                                    Delete account
                                 </button>
                             </div>
                         </div>
                     </div>
                 </div>
+                <Modal
+                    isOpen={this.state.deleteConfirmationModal}
+                    style={DeleteModalStyle}
+                >
+                    <div className='text-2xl'>
+                        <div className='flex'>
+                            <h1 className='mt-2'>Are you sure ?</h1>
+                            <button onClick={this.closeDeleteModal} className='ml-auto p-2 hover:bg-[#202020] rounded-lg'>X</button>
+                        </div>
+                        <div className='grid place-items-center mt-8 text-3xl'>
+                            <button onClick={this.handleSubmitDelete} className='px-5 py-2 rounded-lg bg-gradient-to-r from-[#d71f26] to-[#8b1418] hover:from-[#d15156] hover:to-[#d71f26]' >Confirm</button>
+                        </div>
+                    </div>
+                </Modal>
                 <Modal
                     isOpen={this.state.modalIsOpen}
                     style={ModalStyle}
@@ -192,46 +227,43 @@ class Myaccount extends React.Component {
                     <div className='text-2xl'>
                         <h1 className='pb-5'>Choose your avatar:</h1>
                         <div className='grid grid-cols-3 place-items-center pb-10 space-y-3'>
-                            <button onClick={this.handleChangeUserImage}>
-                                <img className='w-32 h-32 hover:opacity-25' alt='1' src='https://cdn-icons-png.flaticon.com/512/4128/4128170.png' />
+                            <button onClick={e => this.handleChangeUserImage(e, 'https://cdn-icons-png.flaticon.com/512/5089/5089983.png')}>
+                                <img className='w-32 h-32 hover:opacity-25' alt='1' src='https://cdn-icons-png.flaticon.com/512/5089/5089983.png' />
                             </button>
 
-                            <button>
+                            <button onClick={e => this.handleChangeUserImage(e, 'https://cdn-icons-png.flaticon.com/512/4128/4128385.png')}>
                                 <img className='w-32 h-32 hover:opacity-25' alt='2' src='https://cdn-icons-png.flaticon.com/512/4128/4128385.png' />
                             </button>
 
-                            <button>
+                            <button onClick={e => this.handleChangeUserImage(e, 'https://cdn-icons-png.flaticon.com/512/219/219988.png')}>
                                 <img className='w-32 h-32 hover:opacity-25' alt='3' src='https://cdn-icons-png.flaticon.com/512/219/219988.png' />
                             </button>
 
-                            <button>
+                            <button onClick={e => this.handleChangeUserImage(e, 'https://cdn-icons-png.flaticon.com/512/4128/4128226.png')}>
                                 <img className='w-32 h-32 hover:opacity-25' alt='4' src='https://cdn-icons-png.flaticon.com/512/4128/4128226.png' />
                             </button>
 
-                            <button>
+                            <button onClick={e => this.handleChangeUserImage(e, 'https://cdn-icons-png.flaticon.com/512/4128/4128196.png')}>
                                 <img className='w-32 h-32 hover:opacity-25' alt='5' src='https://cdn-icons-png.flaticon.com/512/4128/4128196.png' />
                             </button>
 
-                            <button>
+                            <button onClick={e => this.handleChangeUserImage(e, 'https://cdn-icons-png.flaticon.com/512/206/206853.png')}>
                                 <img className='w-32 h-32 hover:opacity-25' alt='6' src='https://cdn-icons-png.flaticon.com/512/206/206853.png' />
                             </button>
 
-                            <button>
+                            <button onClick={e => this.handleChangeUserImage(e, 'https://cdn-icons-png.flaticon.com/512/4128/4128309.png')}>
                                 <img className='w-32 h-32 hover:opacity-25' alt='7' src='https://cdn-icons-png.flaticon.com/512/4128/4128309.png' />
                             </button>
 
-                            <button>
+                            <button onClick={e => this.handleChangeUserImage(e, 'https://cdn-icons-png.flaticon.com/512/4128/4128273.png')}>
                                 <img className='w-32 h-32 hover:opacity-25' alt='8' src='https://cdn-icons-png.flaticon.com/512/4128/4128273.png' />
                             </button>
 
-                            <button>
+                            <button onClick={e => this.handleChangeUserImage(e, 'https://cdn-icons-png.flaticon.com/512/4128/4128296.png')}>
                                 <img className='w-32 h-32 hover:opacity-25' alt='9' src='https://cdn-icons-png.flaticon.com/512/4128/4128296.png' />
                             </button>
                         </div>
-                        <div className='flex space-x-5 text-center items-center'>
-                            <button className='px-5 py-2 rounded-lg bg-gradient-to-r from-[#8b1418] to-[#d71f26] hover:from-[#d71f26] hover:to-[#d15156]' >Save</button>
-                            <button onClick={this.closeModal} className='px-5 py-2 rounded-lg bg-gradient-to-r from-[#d71f26] to-[#8b1418] hover:from-[#d15156] hover:to-[#d71f26]' >Cancel</button>
-                        </div>
+                        <button onClick={this.closeModal} className='px-5 py-2 rounded-lg bg-gradient-to-r from-[#d71f26] to-[#8b1418] hover:from-[#d15156] hover:to-[#d71f26]' >Cancel</button>
                     </div>
                 </Modal>
                 <ToastContainer toastStyle={{ backgroundColor: '#141414', color: '#FFFFFF' }} />
