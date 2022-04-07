c0f4bc7fd42c004943af3e921e555de4a5f8d2cf
DUBOSCLARD LÉO <leo.dubosclard@epitech.eu>
2022-04-07 18:01:01 +0200

    feat: Add all buttons title
    Signed-off-by: DUBOSCLARD LÉO <leo.dubosclard@epitech.eu>

--- frontend/src/pages/Loggedin.js
+++ frontend/src/pages/Loggedin.js
@@ -67,14 +67,14 @@ export class Loggedin extends React.Component {
                 <div className='flex flex-row align-bottom '>
                     <h1 className='text-2xl'>Movies selection</h1>
                     <Link to='/movies' className='ml-auto'>
-                        <button className='text-red-600 ml-auto transition hover:scale-110'>See all <span className='text-3xl pb-1'> > </span></button>
+                        <button title='See all movies' className='text-red-600 ml-auto transition hover:scale-110'>See all <span className='text-3xl pb-1'> > </span></button>
                     </Link>
                 </div>
                 <div className='mx-10'>
                     <Swiper slidesPerView={7} >
                         {this.state.movies.map(movie => (
                             <SwiperSlide key={movie.show_id} className='flex-shrink-0'>
-                                <button onClick={e => this.handleSubmitMovie(e, movie.id)}>
+                                <button onClick={e => this.handleSubmitMovie(e, movie.id)} title={movie.title}>
                                     <h1 className='text-lg truncate w-52'>{movie.title}</h1>
                                     <img alt={String(movie.show_id)} className='object-cover w-56 h-80' src={movie.picture} />
                                 </button>
@@ -86,14 +86,14 @@ export class Loggedin extends React.Component {
                 <div className='flex flex-row align-bottom pt-5'>
                     <h1 className='text-2xl'>TV Shows selection</h1>
                     <Link to='/tvshows' className='ml-auto'>
-                        <button className='text-red-600 transition hover:scale-110'>See all <span className='text-3xl pb-1'> > </span></button>
+                        <button title='See all tv shows' className='text-red-600 transition hover:scale-110'>See all <span className='text-3xl pb-1'> > </span></button>
                     </Link>
                 </div>
                 <div className='mx-10'>
                     <Swiper slidesPerView={7}>
                         {this.state.tv_shows.map(tv_show => (
                             <SwiperSlide key={tv_show.show_id}  className='flex-shrink-0'>
-                                <button onClick={e => this.handleSubmitMovie(e, tv_show.id)}>
+                                <button onClick={e => this.handleSubmitMovie(e, tv_show.id)} title={tv_show.title}>
                                     <h1 className='text-lg truncate w-52'>{tv_show.title}</h1>
                                     <img className='object-cover w-56 h-80' src={tv_show.picture} alt={String(tv_show.show_id)} />
                                 </button>
