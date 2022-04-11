4a86f6c05e8cfd1c72a12dcf461c2aaac49ffa72
DUBOSCLARD LÉO <leo.dubosclard@epitech.eu>
2022-04-11 10:53:38 +0200

    feat: add Tv Show request to get all tv shows
    Signed-off-by: DUBOSCLARD LÉO <leo.dubosclard@epitech.eu>

--- frontend/src/pages/Tvshow.js
+++ frontend/src/pages/Tvshow.js
@@ -1,14 +1,41 @@
 import React from 'react';
 import { HomeNavbar } from './Navbar';
 
+import isConnected from '../js/isConnected';
+
+import { Navigate } from 'react-router-dom';
+
 export default class Tvshow extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
+            isLoggedIn: isConnected(),
             tvshows: [],
             filter: 'Genre'
         };
         this.handleChange = this.handleChange.bind(this);
+    }
+
+    componentDidMount() {
+        if (this.state.isLoggedIn) {
+            let movie_body = {
+                type: "TV Show"
+            };
+
+            fetch("http://localhost:5001/films/type", {
+                method: "POST",
+                headers: new Headers({
+                    "Authorization": `Bearer ${localStorage.getItem("user_token")}`,
+                    "Content-Type": "application/json"
+                }),
+                mode: "cors",
+                body: JSON.stringify(movie_body)
+            })
+            .then(response => response.json())
+            .then(movies => {
+                this.setState({ tvshows: movies });
+            });
+        }
     }
 
     handleChange(event) {
@@ -16,6 +43,8 @@ export default class Tvshow extends React.Component {
     }
 
     render () {
+        if (!this.state.isLoggedIn)
+            return <Navigate to='/' />;
         return (
             <div className='w-screen h-screen mx-auto text-white bg-[#141414]'>
                 <HomeNavbar />
@@ -29,6 +58,14 @@ export default class Tvshow extends React.Component {
                             <option value='romantic'>Romantic</option>
                         </select>
                     </div>
+                </div>
+                <div id='movies' className='grid grid-cols-5 w-screen'>
+                    {this.state.tvshows.map(tvshow => (
+                        <div className='flex-shrink-0'>
+                            <h1 className='text-lg truncate w-52'>{tvshow.title}</h1>
+                            <img className='object-cover w-56 h-80' src={tvshow.picture} alt={String(tvshow.show_id)} />
+                        </div>
+                    ))}
                 </div>
             </div>
         );
